a40e32375910bfc7de3d10504af9c58849f527c9
Vishal Bhapkar <vishalbhapkar@vishalbhap-ventura.local>
2023-10-05 16:25:40 +0530

    code updated

--- server.js
+++ server.js
@@ -6,23 +6,10 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "Hi Sachin Tendulkar. CI-CD working fine with AWS From Vishal"
+        message: "Hi Mahesh Nagtilak. CI-CD working fine with AWS From Vishal"
     })
 })
 
-app.get('/mahesh', (req, res) => {
-    res.json({
-        status: 200,
-        message: "Hi Mahesh"
-    })
-})
-
-app.get('/mahesh/users', (req, res) => {
-    res.json({
-        status: 200,
-        message: "Hi Mahesh, users loaded."
-    })
-})
 
 app.listen(8080, () => {
     console.log('server running on port 8080')
